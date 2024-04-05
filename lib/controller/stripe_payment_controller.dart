e5ca3fb572aa8088b07d7754d075abaa5148f753
naimulhassan2001 <developernaimul00@gmail.com>
2024-04-05 14:47:24 +0600

    improve code

--- lib/controller/stripe_payment_controller.dart
+++ lib/controller/stripe_payment_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
@@ -10,21 +11,20 @@ class PaymentController {
 
   Future<PaymentIntent> stripeCheckPaymentIntentTransaction(String piId) async {
     try {
-      // Initialize Stripe with your publishable key
       Stripe.publishableKey =
           'pk_test_51OuFaT00cpr6c22EPbiASuCZtGuUhGNbYVPnBWSa7qD1XakSzGclaIXRbOzgkbA4J8IaVHxxjWfVbt2jPRcvcwMc00Eh8xvPlm';
 
-      // Retrieve payment intent using its ID
       final paymentIntent = await Stripe.instance.retrievePaymentIntent(piId);
 
-      // Return the payment intent
-
-      print(
-          "===========================================> paymentIntent $paymentIntent");
+      if (kDebugMode) {
+        print(
+            "===========================================> paymentIntent $paymentIntent");
+      }
       return paymentIntent;
     } catch (e) {
-      // Handle any errors
-      print('Error fetching payment intent: $e');
+      if (kDebugMode) {
+        print('Error fetching payment intent: $e');
+      }
       throw e;
     }
   }
@@ -36,31 +36,36 @@ class PaymentController {
         await Stripe.instance
             .initPaymentSheet(
                 paymentSheetParameters: SetupPaymentSheetParameters(
-          // billingDetails: const BillingDetails(
-          //     name: 'Siam Prodhan',
-          //     email: 'siamjht@gmail.com',
-          //     phone: '01712345678',
-          //     address: Address(
-          //         city: 'New York City',
-          //         country: 'United States',
-          //         line1: 'New York City',
-          //         line2: 'New York City',
-          //         postalCode: '12345',
-          //         state: 'New York City')
-          // ),
-          // googlePay: const PaymentSheetGooglePay(merchantCountryCode: 'US'),
+          billingDetails: const BillingDetails(
+              name: 'Naimul Hassan',
+              email: 'developernaimul00@gmail.com',
+              address: Address(
+                  city: 'Dhaka',
+                  country: 'Bangladesh',
+                  line1: 'Dhaka',
+                  line2: 'Dhaka',
+                  postalCode: '1121',
+                  state: 'Dhaka')),
+          googlePay: const PaymentSheetGooglePay(merchantCountryCode: 'US'),
           merchantDisplayName: 'Naimul Hassan',
           paymentIntentClientSecret: paymentIntentData!['client_secret'],
           style: ThemeMode.dark,
         ))
             .then((value) {
-          print('Is completed payment properly????????????????');
+          if (kDebugMode) {
+            print('Is completed payment properly????????????????');
+          }
         });
         displayPaymentSheet();
-        print("----------display payment sheet didn't called");
+        if (kDebugMode) {
+          print("----------display payment sheet didn't called");
+        }
       }
     } catch (e, s) {
-      print('-=-=-=-=-=-=-=---=-=-=-exception: $e $s =-=-=-=-=--=-=-=-=-=-=-=');
+      if (kDebugMode) {
+        print(
+            '-=-=-=-=-=-=-=---=-=-=-exception: $e $s =-=-=-=-=--=-=-=-=-=-=-=');
+      }
     }
   }
 
@@ -74,21 +79,21 @@ class PaymentController {
       var response = await http.post(
           Uri.parse('https://api.stripe.com/v1/payment_intents'),
           body: body,
-          // headers: {
-          //   'Authorization' : 'Bearer sk_test_51NsL1qAGQlA3WKiItI5TaZRQlYgxUxxJatB27Dqp9MeBJQo1AH7LrQ1VK2FSYLfgLPXFKOpp3no9ii8XEgSbfgPV008CBSw9Z8',
-          //   'Content-Type' : 'application/x-www-form-urlencoded'
-          // }
           headers: {
             'Authorization':
                 'Bearer sk_test_51OuFaT00cpr6c22EaepfzgtLDqaPXVJKCpGVGFvF3LjhmPMZg02U7HgYMzpPlpi2T5w4EmjFsAkVmry1ZYlweyXu00T5ujqx3N',
             'Content-Type': 'application/x-www-form-urlencoded'
           });
-      print(
-          "=============>>>${response.body}----${response.statusCode}<<<==================");
+      if (kDebugMode) {
+        print(
+            "=============>>>${response.body}----${response.statusCode}<<<==================");
+      }
 
       return jsonDecode(response.body);
     } catch (e) {
-      print("Error Occurred:======================> ${e.toString()}");
+      if (kDebugMode) {
+        print("Error Occurred:======================> ${e.toString()}");
+      }
     }
   }
 
@@ -101,17 +106,17 @@ class PaymentController {
     try {
       await Stripe.instance.presentPaymentSheet();
       stripeCheckPaymentIntentTransaction(paymentIntentData!['client_secret']);
-      print('payment intent' + paymentIntentData!['client_secret'].toString());
-      print('payment intent' + paymentIntentData!['client_secret'].toString());
-      print('payment intent' + paymentIntentData!['amount'].toString());
-      print('payment intent' + paymentIntentData.toString());
-      //orderPlaceApi(paymentIntentData!['id'].toString());
+      if (kDebugMode) {
+        print('payment intent$paymentIntentData');
+      }
       ScaffoldMessenger.of(Get.context!)
           .showSnackBar(const SnackBar(content: Text("paid successfully")));
 
       paymentIntentData = null;
     } catch (e) {
-      print("error $e");
+      if (kDebugMode) {
+        print("error $e");
+      }
     }
   }
 }
